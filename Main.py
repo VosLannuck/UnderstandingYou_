02589260afc21f4aacfddc9a0cded40f205709c2
vos <voslannack@gmail.com>
2023-12-07 22:20:36 +0700

    ADd more options

--- Main.py
+++ Main.py
@@ -1,41 +1,81 @@
 import torch
 import argparse
+import mlflow
 
 from Baseline_CNN import CNN_Baseline
 from Baseline_Resnet import BaselineResnet, ResidualBlock
 from Baseline_VGG import VGG_Baseline_16
-
+from DatasetMaker import MainSmokerDataset
 from Trainer import Trainer
 
 from sklearn.metrics import classification_report
-from torch.utils.data import random_split, DataLoader
+from torch.utils.data import DataLoader
+from torch.nn import CrossEntropyLoss
 from torch.optim import AdamW
+from torchsummary import summary
+
+from typing import Dict, Any
+import os
 
 INIT_LR: float = 1e-3
 BATCH_SIZE: int = 64
 EPOCH_SIZE: int = 10
+IMG_SIZE: int = 64
 
 TRAIN_SPLIT: float = 0.75
 VAL_SPLIT: float = 1 - TRAIN_SPLIT
 NUM_CLASSES: int = 2
 
-device: str = torch.device("cuda" if torch.cuda.is_available() else "cpua")
+TRAINING_PATH: str = "Dataset/Training/"
+VALIDATION_PATH: str = "./Dataset/Validation/"
+TESTING_PATH: str = "./Dataset/Testing/"
+
+device: str = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 arg: argparse.ArgumentParser = argparse.ArgumentParser()
-arg.add_argument("-m", "--model", type=str, required=True,
-                 help="Path to output trained model")
-arg.add_argument("-p", "--plot", type=str, required=True,
-                 help="Path to output loss/accracy plot")
+arg.add_argument("-m", "--model", type=str,
+                 help="Path to output trained model", default="m")
+arg.add_argument("-p", "--plot", type=str,
+                 help="Path to output loss/accracy plot", default="m")
 arg.add_argument("-b", "--batch", type=int, default=BATCH_SIZE)
 arg.add_argument("-e", "--epoch", type=int, default=EPOCH_SIZE)
 arg.add_argument("-lr", "--learning_rate", type=int, default=INIT_LR)
-
+arg.add_argument("-trp", "--training_path", type=str, default=TRAINING_PATH)
+arg.add_argument("-vp", "--valid_path", type=str, default=VALIDATION_PATH)
+arg.add_argument("-tsp", "--test_path", type=str, default=TESTING_PATH)
+arg.add_argument("-nc", "--num_classes", type=str, default=2)
+arg.add_argument("-img", "--image_size", type=int, default=IMG_SIZE)
 args = vars(arg.parse_args())
-print(args)
 
+cnnModel: CNN_Baseline = CNN_Baseline(3, classes=NUM_CLASSES)
+cnnModel.to(device)
+summary(cnnModel, (3, IMG_SIZE, IMG_SIZE))
 
-cnnModel: CNN_Baseline = CNN_Baseline(128, classes=NUM_CLASSES)
-vggModel: VGG_Baseline_16 = VGG_Baseline_16(numClasses=NUM_CLASSES)
-resnetMode: BaselineResnet = BaselineResnet()
+# vggModel: VGG_Baseline_16 = VGG_Baseline_16(numClasses=NUM_CLASSES)
+# resnetMode: BaselineResnet = BaselineResnet()
+mainSmokerDatasetMaker: MainSmokerDataset = MainSmokerDataset(
+    trainData=args['training_path'],
+    validationData=args['valid_path'],
+    testingData=args['test_path'],
+    numClasses=args['num_classes'],
+    columns_df=["img_path", "class_name", "label"],
+)
 
+mainSmokerDatasetMaker.makeDataFrame(args["training_path"])
+mainSmokerDatasetMaker.makeDataFrame(args["valid_path"])
+mainSmokerDatasetMaker.makeDataFrame(args["test_path"])
+mainSmokerDatasetMaker.makeDataset()
+mainSmokerDatasetMaker.makeDataLoader(batchSize=BATCH_SIZE)
 
+trainDataLoader: DataLoader = mainSmokerDatasetMaker.trainDataLoader
+validDataLoader: DataLoader = mainSmokerDatasetMaker.validationDataLoader
+testDataLoader: DataLoader = mainSmokerDatasetMaker.testDataLoader
+
+trainer: Trainer = Trainer(
+    trainDataLoader,
+    validDataLoader,
+    testDataLoader,
+)
+cls = CrossEntropyLoss()
+trainer.TrainModel(cnnModel, cls,
+                   AdamW(params=cnnModel.parameters(), lr=INIT_LR))
--- Trainer.py
+++ Trainer.py
@@ -1,115 +1,106 @@
-#%%
 import torch
-import mlflow
 import os
 import numpy as np
 
-from DatasetMaker import MainSmokerDataset, SmokerDataset
 from torch.utils.data import DataLoader
-from torch.nn import CrossEntropyLoss
+from torch.nn import (CrossEntropyLoss, Module)
 from tqdm import tqdm
 from typing import List, Tuple, Dict
 
-#%%
-device : str = "cuda" if torch.cuda.is_available() else "cpu" 
+device: str = "cuda" if torch.cuda.is_available() else "cpu"
+
+
 class Trainer():
     def __init__(self,
-                 trainDataLoader : DataLoader,
-                 validationDataLoader : DataLoader,
-                 testDataLoader : DataLoader,
+                 trainDataLoader: DataLoader,
+                 validationDataLoader: DataLoader,
+                 testDataLoader: DataLoader,
                  ):
-        self.trainDataLoader : DataLoader = trainDataLoader
-        self.validationDataLoader : DataLoader = validationDataLoader
-        self.testDataLoader : DataLoader = testDataLoader
-        
-        self.logs : Dict[str, List[float]] = {
-            "train_loss" : [],
-            "train_acc" : [],
-            "val_loss" : [],
-            "val_acc" : [],
-        }       
-        
-    def train_step(self, model : torch.nn.Module, loss_fn : CrossEntropyLoss , optimizer : torch.optim.Optimizer) -> Tuple[torch.Tensor, torch.Tensor]:
-        totalObservation : int = 0
-        currentLoss : float = 0.0
-        correct_prediction : int = 0
-        
-        numBatches : int = len(self.trainDataLoader)
-        model.train()
+        self.trainDataLoader: DataLoader = trainDataLoader
+        self.validationDataLoader: DataLoader = validationDataLoader
+        self.testDataLoader: DataLoader = testDataLoader
+        self.logs: Dict[str, List[float]] = {
+            "train_loss": [],
+            "train_acc": [],
+            "val_loss": [],
+            "val_acc": [],
+        }
 
+    def train_step(self, model: Module, loss_fn: CrossEntropyLoss,
+                   optimizer: torch.optim.Optimizer
+                   ) -> Tuple[torch.Tensor, torch.Tensor]:
+        totalObservation: int = 0
+        currentLoss: float = 0.0
+        correct_prediction: int = 0
+        numBatches: int = len(self.trainDataLoader)
+        model.train()
         for data, target in self.trainDataLoader:
-            target : torch.Tensor = target.type(torch.LongTensor)
+            target: torch.Tensor = target.type(torch.LongTensor)
             data, target = data.to(device), target.to(device)
-            
             # Clean previous calculations
             optimizer.zero_grad()
-            
             # Forward prop
-            outputs : torch.Tensor = self.model(data)
-            
-            loss : torch.Tensor = loss_fn(outputs, target)
-            
-            ## Back prop
+            outputs: torch.Tensor = model(data)
+            loss: torch.Tensor = loss_fn(outputs, target)
+            # Back prop
             loss.backward()
-
-            ## Optimzing model
+            # Optimzing model
             optimizer.step()
 
             currentLoss += loss.item()
-            _, pred_indices = torch.max(outputs, dim=1) # [0 , 1]
+            _, pred_indices = torch.max(outputs, dim=1)  # [0 , 1]
             correct_prediction += torch.sum(pred_indices == target).item()
             totalObservation += target.shape[0]
-        
         return correct_prediction / totalObservation, currentLoss / numBatches
-            
-            
-    def test_or_eval_step(self, model : torch.nn.Module,loss_fn : CrossEntropyLoss, dataLoader : DataLoader ) -> Dict[torch.Tensor, torch.Tensor]:
-        totalObservation : int = 0
-        currentLossTotal : float = 0.0
-        num_batches : int = len(dataLoader)
-        correct_prediction : int = 0 
+
+    def test_or_eval_step(self, model: Module,
+                          loss_fn: CrossEntropyLoss, dataLoader: DataLoader
+                          ) -> Dict[torch.Tensor, torch.Tensor]:
+        totalObservation: int = 0
+        currentLossTotal: float = 0.0
+        num_batches: int = len(dataLoader)
+        correct_prediction: int = 0
         with torch.no_grad():
             model.eval()
-            
             for data, target in dataLoader:
-                target : torch.Tensor = target.type(torch.LongTensor)
-                data : torch.Tensor = data.to(device)
+                print(data.shape)
+                target: torch.Tensor = target.type(torch.LongTensor)
+                # Convert to gpu
+                data: torch.Tensor = data.to(device)
                 target = target.to(device)
-                
-                outputs : torch.Tensor = model(data)
-                
-                loss : torch.Tensor = loss_fn(outputs, target)
-                
+                # calculate output loss
+                outputs: torch.Tensor = model(data)
+                loss: torch.Tensor = loss_fn(outputs, target)
                 currentLossTotal += loss.item()
-                
-                ## Get the max from the outputs predictions, that is why dim is 1 not 0
-                _, predIndices = torch.max(outputs,dim=1) 
-                correct_prediction += torch.sum(target == predIndices).item() 
-                totalObservation += 1
-            
-        return correct_prediction / totalObservation, currentLossTotal / num_batches
-    
-    def TrainModel(self, model : torch.nn.Module, loss_fn : CrossEntropyLoss, optimizer : torch.optim.Optimizer,
-                   epoch : int = 5 , lr : float = 1e-4, checkpointPath : str = "checkpoints"):
-        if(not os.path.isdir(checkpointPath)):
+                # Get the max from the outputs predictions,
+                # that is why dim is 1 not 0
+                _, predIndices = torch.max(outputs, dim=1)
+                correct_prediction += torch.sum(target == predIndices).item()
+                totalObservation += target.shape[0]
+        avgCorrect: int = correct_prediction / totalObservation
+        avgLoss: float = currentLossTotal / num_batches
+        print(currentLossTotal)
+        return avgCorrect, avgLoss
+
+    def TrainModel(self, model: Module, loss_fn: CrossEntropyLoss,
+                   optimizer: torch.optim.Optimizer, epoch: int = 5,
+                   lr: float = 1e-3, checkpointPath: str = "checkpoints"):
+        if (not os.path.isdir(checkpointPath)):
             os.mkdir(checkpointPath)
-        
         model.to(device)
-        best_loss : float = np.inf
-        
+        best_loss: float = np.inf
         for epoch in tqdm(range(epoch)):
             train_acc, train_loss = self.train_step(model, loss_fn, optimizer)
             val_acc, val_loss = self.test_or_eval_step(model, loss_fn, self.validationDataLoader)
-            print("Epoch : %g "% (epoch))
+            print("Epoch : %g " % (epoch))
             print(f"TrainLoss : {train_loss:.4f}, TrainAcc : {train_acc:.4f}")
-            print(f"ValLoss : {val_loss:.4f}, ValAcc : {val_loss:.4f}")
-
+            print(f"ValLoss : {val_loss:.4f}, ValAcc : {val_acc:.4f}")
+            # Log
             self.logs["train_loss"].append(train_loss)
             self.logs["train_acc"].append(train_acc)
-            
             self.logs["val_loss"].append(val_loss)
             self.logs["val_acc"].append(val_acc)
-            
+            # Model Saving
             torch.save(model.state_dict(), "checkpoints/last.pth")
             if val_loss < best_loss:
                 best_loss = val_loss
