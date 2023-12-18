22d096deb15d22441ed3fa27cc67b96ec8168901
vos <voslannack@gmail.com>
2023-12-18 16:43:49 +0700

    smaller

--- ModelBuilder.py
+++ ModelBuilder.py
@@ -55,7 +55,7 @@ def preserveModel(modelName: ModelName,
                                                      numClasses=num_classes).to(device)
         return resnetModel
     elif (modelName == ModelName.vit):
-        vit_model: SwinTransformer = models.swin_v2_s(weights='DEFAULT').to(device)
+        vit_model: SwinTransformer = models.swin_v2_t(weights='DEFAULT').to(device)
         vit_model.head = Linear(in_features=vit_model.head.in_features,
                                 out_features=num_classes).to(device)
         return vit_model
