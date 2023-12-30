aac8b457094fe5aac0d88ae432bdeb54bcdeff1a
vos <voslannack@gmail.com>
2023-12-30 18:53:18 +0700

    fix identation error

--- ErrorAnalysisUtils.py
+++ ErrorAnalysisUtils.py
@@ -1,5 +1,7 @@
 import pandas as pd
-import numpy as np import matplotlib.pyplot as plt import torch
+import numpy as np 
+import matplotlib.pyplot as plt 
+import torch
 import FeatureMapsExtractor as fme
 from Enums import ModelName, ModelMethod
 from torch.nn import Module
